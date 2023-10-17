99dbe3f16e201386063a6476c2d1042291b756a1
YaroslavZuban <72079338+YaroslavZuban@users.noreply.github.com>
2023-10-17 14:56:54 +0700

    Форматирования кода

--- src/main/java/com/eltex/main/Main.java
+++ src/main/java/com/eltex/main/Main.java
@@ -4,10 +4,10 @@ import com.eltex.service.WallService;
 
 public class Main {
     public static void main(String[] args) {
-        final var service1=new WallService();
+        final var service1 = new WallService();
         System.out.println(service1);
 
-        final var service2=new WallService();
+        final var service2 = new WallService();
         System.out.println(service2);
     }
 }
