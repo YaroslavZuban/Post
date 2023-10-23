ab4afbfe49055a9dc2c2f8b9cd36fcad03e67b72
YaroslavZuban <72079338+YaroslavZuban@users.noreply.github.com>
2023-10-23 11:10:42 +0700

    Задача №1. Post Advanced

---
+++ src/main/java/com/eltex/post/Coordinates.java
@@ -0,0 +1,21 @@
+package com.eltex.post;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Coordinates {
+    private double latitude;
+    private double longitude;
+
+    public Coordinates(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+}
